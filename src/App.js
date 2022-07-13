ec8ffbbd577121b2be79b5c6ee38c5803841e7a7
Adikey <108686012+Adikey@users.noreply.github.com>
2022-07-13 16:22:32 +0600

    added changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieCard from './MovieCard';
 import './App.css';
 import SearchIcon from './search.svg';
 
-const API_URL = 'http://www.omdbapi.com?apikey=bcc1d5b9'
+const API_URL = 'https://www.omdbapi.com?apikey=bcc1d5b9'
 
 
 const App = () => {

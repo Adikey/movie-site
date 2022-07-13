947999361b15a891d74f8287a2808692e82d0a6a
Adikey <108686012+Adikey@users.noreply.github.com>
2022-07-13 15:42:41 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,14 +7,6 @@ import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=bcc1d5b9'
 
-const movie1 = {
-    "Title": "Amazing Spiderman Syndrome",
-    "Year": "2012",
-    "imdbID": "tt2586634",
-    "Type": "movie",
-    "Poster": "N/A"
-}
-
 
 const App = () => {
   const [movies, setMovies] = useState([]);
